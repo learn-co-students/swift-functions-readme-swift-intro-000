c08594846b2029f1de56a33ef4e350b2876d70d6
Dane Wallace <danewallace@icloud.com>
2017-11-04 20:34:47 -0400

    Removed extraneous comments and line

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,44 +1,12 @@
-func sayHello() {
-    let greeting = "Hello there!"
+func sayHello(name: String) {
+    let greeting = "Hello \(name), why do you sleep so much?"
     print(greeting)
 }
 
-sayHello()
-sayHello()
-
-// Uncomment this line to see the error that is printed in the console.
-// (You can uncomment a line by removing the // at the beginning of the line.)
-//print(greeting)
-
-
-func sayHelloToGarfield() {
-    print("Hello, Garfield, why do you sleep so much?")
-}
-
-func sayHelloToBuster() {
-    print("Hello, Buster, why do you sleep so much?")
-}
-
-func sayHelloToScratchy() {
-    print("Hello, Scratchy, why do you sleep so much?")
-}
-
-
-func sayHelloToCat(catName: String) {
-    print("Hello, \(catName), why do you sleep so much?")
-}
-
-
-func sayHello(name: String) {
-    print("Hello \(name), why do you sleep so much?")
-}
-
 sayHello(name: "Mittens")
-// Prints "Hello Mittens, why do you sleep so much?"
-
-sayHello(name: "Socks")
-// Prints "Hello Socks, why do you sleep so much?"
 
 let dumpsterCat = "Rocky"
+
 sayHello(name: dumpsterCat)
-// Prints "Hello Rocky, why do you sleep so much?"
+
+
